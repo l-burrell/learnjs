1c2124db4f142ffb86830558f67e94716eab3193
burrell <eewinterz@gmail.com>
2019-09-21 14:05:20 -0400

    testing js alert and prompt

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,29 @@
-var firstName = "John";
-var lastName = "Smith";
-console.log(firstName + " " + lastName);
+// //variables and data types
+// var firstName = "John";
+// var lastName = "Smith";
+// console.log(firstName + " " + lastName);
 
-var age = 19;
+// var age = 19;
+// console.log(age);
+
+// var isAdult = age > 18;
+// console.log(isAdult);
+
+// //Variable naming rules
+// var _3years = 3;
+// var johnMark = "John and Mark";
+
+
+var firstName = 'john';
+var job, isMarried;//common practice
+job = 'teacher';
+isMarried = false;
+var wasHeMarried = isMarried ? 'yes' : 'no';
+
+console.log(firstName + " is a " + job + ". Is he married? " + wasHeMarried);
+
+var age = "twenty eight";
 console.log(age);
+// alert(wasHeMarried);
+var lastName = prompt("what is his last name?");
+console.log(lastName)
